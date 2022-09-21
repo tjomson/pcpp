9579c4c4ca7cd7cee70b7be6c7defb43b677fde8
Thomas Hoffmann Kilbak <thki@queue-it.com>
2022-09-21 14:50:39 +0200

    Add boundedbuffer

--- week03/code-exercises/week03exercises/app/src/main/java/exercises03/BoundedBufferInteface.java
+++ week03/code-exercises/week03exercises/app/src/main/java/exercises03/BoundedBufferInteface.java
@@ -2,7 +2,8 @@
 // raup@itu.dk * 18/09/2021
 package exercises03;
 
-interface BoundedBufferInteface<T> {
+interface BoundedBufferInterface<T> {
     public T take() throws Exception;
+
     public void insert(T elem) throws Exception;
 }
