fdeb7298856c93b871e5ed23998da983658e65e5
Thomas Hoffmann Kilbak <thki@queue-it.com>
2022-09-21 13:05:23 +0200

    Add covid

--- week01/code-exercises/week01exercises/app/src/main/java/exercises01/CounterThreads2Covid.java
+++ week01/code-exercises/week01exercises/app/src/main/java/exercises01/CounterThreads2Covid.java
@@ -2,38 +2,45 @@
 // raup@itu.dk * 2021-08-27
 package exercises01;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CounterThreads2Covid {
 
-    long counter = 0;
-    final long PEOPLE  = 10_000;
-    final long MAX_PEOPLE_COVID = 15_000;
+	ReentrantLock lock = new ReentrantLock();
 
-    public CounterThreads2Covid() {
-	try {
-	    Turnstile turnstile1 = new Turnstile();
-	    Turnstile turnstile2 = new Turnstile();
+	long counter = 0;
+	final long PEOPLE = 10_000;
+	final long MAX_PEOPLE_COVID = 15_000;
 
-	    turnstile1.start();turnstile2.start();
-	    turnstile1.join();turnstile2.join();
+	public CounterThreads2Covid() {
+		try {
+			Turnstile turnstile1 = new Turnstile();
+			Turnstile turnstile2 = new Turnstile();
 
-	    System.out.println(counter+" people entered");
+			turnstile1.start();
+			turnstile2.start();
+			turnstile1.join();
+			turnstile2.join();
+
+			System.out.println(counter + " people entered");
+		} catch (InterruptedException e) {
+			System.out.println("Error " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
-	catch (InterruptedException e) {
-	    System.out.println("Error " + e.getMessage());
-	    e.printStackTrace();
+
+	public static void main(String[] args) {
+		new CounterThreads2Covid();
 	}
-    }
 
-    public static void main(String[] args) {
-	new CounterThreads2Covid();
-    }
-
-
-    public class Turnstile extends Thread {
-	public void run() {
-	    for (int i = 0; i < PEOPLE; i++) {
-		counter++;
-	    }	    
+	public class Turnstile extends Thread {
+		public void run() {
+			for (int i = 0; i < PEOPLE; i++) {
+				lock.lock();
+				if (counter < MAX_PEOPLE_COVID)
+					counter++;
+				lock.unlock();
+			}
+		}
 	}
-    }
 }
