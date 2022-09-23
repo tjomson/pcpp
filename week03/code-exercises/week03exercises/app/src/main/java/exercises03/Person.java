87e47deecc003a65b49f5558f2880eef4c5ac334
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-09-23 10:58:30 +0200

    Update Person.java

--- week03/code-exercises/week03exercises/app/src/main/java/exercises03/Person.java
+++ week03/code-exercises/week03exercises/app/src/main/java/exercises03/Person.java
@@ -1,44 +1,38 @@
 package exercises03;
 
-import java.util.concurrent.locks.ReentrantLock;
-
 public class Person {
     private static long _id = -1;
-    private static ReentrantLock class_lock = new ReentrantLock();
 
     public final long id;
     private String name;
     private int zip;
     private String address;
-    private ReentrantLock instance_lock = new ReentrantLock();
 
     public Person() {
-        class_lock.lock();
-        instance_lock.lock();
-        id = ++_id;
-        instance_lock.unlock();
-        class_lock.unlock();
+        synchronized (Person.class) {
+            synchronized (this) {
+                id = ++_id;
+            }
+        }
     }
 
     public Person(long i) {
-        class_lock.lock();
-        instance_lock.lock();
-        if (_id == -1) {
-            id = i;
-            _id = i;
+        synchronized (Person.class) {
+            synchronized (this) {
+                if (_id == -1) {
+                    id = i;
+                    _id = i;
+                }
+                else {
+                    id = ++_id;
+                }
+            }
         }
-        else {
-            id = ++_id;
-        }
-        instance_lock.unlock();
-        class_lock.unlock();
     }
 
-    public void changeZipAddr(int n_zip, String n_addr) {
-        instance_lock.lock();
+    public synchronized void changeZipAddr(int n_zip, String n_addr) {
         zip = n_zip;
         address = n_addr;
-        instance_lock.unlock();
     }
 
     public int getZip() { return zip; }
