4f5a6ff5080f831df07fd68ade050659bb46e7d5
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-09-23 12:31:15 +0200

    3.3

---
+++ week03/code-exercises/week03exercises/app/src/main/java/exercises03/SeeMyFord.java
@@ -0,0 +1,66 @@
+package exercises03;
+
+public class SeeMyFord {
+    private long permits = Long.MIN_VALUE;
+    private boolean hasWaiting = false;
+
+    public SeeMyFord(long init_permits) {
+        permits = init_permits;
+    }
+
+    public synchronized void acquire() throws InterruptedException {
+
+        if (permits <= 0) {
+            hasWaiting = true;
+            wait();
+        }
+        permits--;
+        return;
+    }
+
+    public synchronized boolean isEmpty() {
+        return permits == 0;
+    }
+
+    public synchronized void release() {
+        permits++;
+        if (hasWaiting) {
+            hasWaiting = false;
+            notifyAll();
+        }
+        return;
+    }
+
+    public static void main(String[] args) {
+        SeeMyFord smf = new SeeMyFord(2);
+
+        Thread t1 = new Thread(() -> {
+            try {
+                smf.acquire();
+                System.out.println("T1");
+            } catch ( InterruptedException e ) {
+                System.out.println("T1 fail");
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            try {
+                smf.acquire();
+                System.out.println("T2");
+            } catch ( InterruptedException e ) {
+                System.out.println("T2 fail");
+            }
+        });
+        Thread t3 = new Thread(() -> {
+            try {
+                smf.acquire();
+                System.out.println("T3");
+            } catch ( InterruptedException e ) {
+                System.out.println("T3 fail");
+            }
+        }); 
+
+        t1.start();
+        t2.start();
+        t3.start();
+    }
+}
