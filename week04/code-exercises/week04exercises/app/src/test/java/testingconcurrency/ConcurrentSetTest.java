129307eb5193b67c9e57cfc39334b39e388b7154
emiljapelt <55390848+emiljapelt@users.noreply.github.com>
2022-09-28 14:01:35 +0200

    Work on week04

--- week04/code-exercises/week04exercises/app/src/test/java/testingconcurrency/ConcurrentSetTest.java
+++ week04/code-exercises/week04exercises/app/src/test/java/testingconcurrency/ConcurrentSetTest.java
@@ -1,7 +1,15 @@
 package testingconcurrency;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Semaphore;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 // TODO: Very likely you need to expand the list of imports
+import org.junit.jupiter.api.Test;
 
 public class ConcurrentSetTest {
 
@@ -23,4 +31,87 @@ public class ConcurrentSetTest {
     }
 
     // TODO: Define your tests below
+
+    private Thread set_adder(int a, CyclicBarrier start, Semaphore finish) {
+        return new Thread(() -> {
+            try {
+                start.await();
+                set.add(a);
+                finish.release();
+            } catch (Exception e) {
+                System.out.println("A set_adder failed");
+                return;
+            }
+        });
+    }
+
+    private Thread set_remover(int a, CyclicBarrier start, Semaphore finish) {
+        return new Thread(() -> {
+            try {
+                start.await();
+                set.remove(a);
+                finish.release();
+            } catch (Exception e) {
+                System.out.println("A set_remover failed");
+                return;
+            }
+        });
+    }
+
+    @Test
+    @RepeatedTest(5000)
+    public void add_ConcurrentlyCorrect() {
+        // Parameters
+        int thread_count = 32;
+        int unique_elements = 10;
+
+        // Setup
+        CyclicBarrier start_barrier = new CyclicBarrier(thread_count);
+        Semaphore finish = new Semaphore((-thread_count)+1);
+
+        // Start execution
+        for (int i = 0; i < thread_count; i++) {
+            set_adder(i % unique_elements, start_barrier, finish).start();
+        }
+
+        // Wait for the threads to finish
+        try {
+            finish.acquire();
+        } catch (Exception e) {
+            System.out.println("Test failure");
+            assertFalse(true);
+            return;
+        }
+            
+        assertEquals(unique_elements, set.size());
+    }
+
+    @Test
+    @RepeatedTest(5000)
+    public void remove_ConcurrentlyCorrect() {
+        // Parameters
+        int remove_times = 2;
+        int unique_elements = 10;
+
+        // Setup
+        CyclicBarrier start_barrier = new CyclicBarrier(remove_times*unique_elements);
+        Semaphore finish = new Semaphore((-(remove_times*unique_elements))+1);
+        for (int i = 0; i < unique_elements; i++) set.add(i);
+
+        // Start execution
+        for (int i = 0; i < (remove_times*unique_elements); i++) {
+            set_remover(i % unique_elements, start_barrier, finish).start();
+        }
+
+        // Wait for the threads to finish
+        try {
+            finish.acquire();
+        } catch (Exception e) {
+            System.out.println("Test failure");
+            assertFalse(true);
+            return;
+        }
+
+        assertEquals(0, set.size());
+    }
 }
